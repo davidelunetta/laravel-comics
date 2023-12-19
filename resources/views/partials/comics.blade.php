0bed2d6be842bcf97cffec856606e3cea56970ba
davidelunetta <davidelunetta.27@gmail.com>
2023-12-19 17:03:09 +0100

    modified layout

--- resources/views/partials/comics.blade.php
+++ resources/views/partials/comics.blade.php
@@ -3,14 +3,16 @@
 @section('content')
 
     <h1>DC Comics</h1>
-    <div class="container">
+    <div class="d-flex position-relative">
         @foreach($comics as $comic)
-            <div class="comic">
-                <h2>{{ $comic['title'] }}</h2>
-                <img src="{{ $comic['thumb']}}" alt="cover">
-                <p>{{ $comic['description'] }}</p>
-                <p>Prezzo: {{ $comic['price'] }}</p>
-            </div>
+           
+                <img src="{{ $comic['thumb']}}" class="flex-shrink-0 me-3" alt="cover">
+                <div>
+                  <h5 class="mt-0">{{ $comic['title'] }}</h5>
+                  <p>{{ $comic['description'] }}</p>
+                  <p>Prezzo: {{ $comic['price'] }}</p>
+                  {{-- <a href="#" class="stretched-link">Go somewhere</a> --}}
+                </div>
         @endforeach
     </div>
 @endsection
