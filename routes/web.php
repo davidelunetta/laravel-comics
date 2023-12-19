565f2dbb0f9ad5655a807f4344edd8bdceaa9972
davidelunetta <davidelunetta.27@gmail.com>
2023-12-19 16:52:23 +0100

    modified route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
+});
+
+Route::get('/comics', function () {
+    $comicsFile = public_path('comics.php');
+    if (File::exists($comicsFile)) {
+        $comics = include $comicsFile;
+        return view('partials.comics', compact('comics'));
+    } 
 });
